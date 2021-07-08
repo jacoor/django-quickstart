96e078344ed2864853eb256208c97a328cb27054
Jacek Ostański <jacek@ivolution.pl>
2021-07-08 09:40:57 +0200

    view from template

--- quickstart/settings.py
+++ quickstart/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+PROJECT_APPS = ["www.apps.WwwConfig"]
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -37,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-]
+] + PROJECT_APPS
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- www/views.py
+++ www/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse("Hello World!")
+    context = {
+        "message": "Hello from view!",
+    }
+    return render(request, "www/index.html", context)
