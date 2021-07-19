efe96795e2c0225bc2b0b31e73a7cc35079abde3
Jacek Ostański <jacek@ivolution.pl>
2021-07-19 09:45:55 +0200

    katalog produktów

--- www/admin.py
+++ www/admin.py
@@ -1,21 +1,48 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Footer, Article, Hero
+from .models import Footer, Article, Hero, ProductCategory, Product
 
 
-class ArticleAdmin(admin.ModelAdmin):
+class DateHierarchy(admin.ModelAdmin):
     date_hierarchy = "created"
 
 
-class FooterAdmin(admin.ModelAdmin):
-    date_hierarchy = "created"
+class ArticleAdmin(DateHierarchy):
+    pass
 
 
-class HeroAdmin(admin.ModelAdmin):
-    date_hierarchy = "created"
+class FooterAdmin(DateHierarchy):
+    pass
+
+
+class HeroAdmin(DateHierarchy):
+    pass
+
+
+# products
+
+
+class ProductCategoryAdmin(DateHierarchy):
+    pass
+
+
+class ProductAdmin(DateHierarchy):
+    # handling slug: https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#django.contrib.admin.ModelAdmin.prepopulated_fields
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = [
+        "name",
+    ]
+    list_filter = ["category", "is_featured", "is_top", "is_active"]
+    list_display = ["name", "price", "is_active"] + list_filter
+
+
+# /products
 
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Footer, FooterAdmin)
 admin.site.register(Hero, HeroAdmin)
+
+admin.site.register(ProductCategory, ProductCategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- www/models.py
+++ www/models.py
@@ -33,3 +33,39 @@ class Article(DatedContent, TitledContent):
 
 class Hero(DatedContent, TitledContent):
     image = models.ImageField(upload_to="www/media/hero")
+
+
+# products
+
+
+class ProductCategory(DatedContent):
+    name = models.CharField(max_length=255, blank=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Product categories"
+
+
+class ActiveManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().exclude(is_active=False)
+
+
+class Product(DatedContent):
+    # https://docs.djangoproject.com/en/3.2/ref/models/fields/#slugfield
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    slug = models.SlugField(help_text="used in urls")
+    is_active = models.BooleanField(default=True, help_text="allows to hide product")
+    name = models.CharField(max_length=255, blank=False)
+    short_description = models.CharField(max_length=255, blank=True)
+    descripion = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_featured = models.BooleanField(default=False)
+    is_top = models.BooleanField(default=False)
+
+    active_objects = ActiveManager()
+
+    def __str__(self):
+        return self.name
