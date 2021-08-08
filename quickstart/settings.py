9ee3977dd63c7430a3030737e7a27943bc25a164
Jacek Ostański <jacek@ivolution.pl>
2021-08-08 17:49:43 +0200

    production static hurts

--- quickstart/settings.py
+++ quickstart/settings.py
@@ -168,9 +168,9 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # production
-STATIC_URL = "/static/"
+STATIC_URL = "/static"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
