9158ef07ed9e6e96b4cf626d8a62072f5406cde3
Jacek Ostański <jacek@ivolution.pl>
2021-08-08 21:36:15 +0200

    final

--- heroku-release-tasks.sh
+++ heroku-release-tasks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python manage.py collectstatic --noinput
-ls -al /app/static/
-ls -al /app/static/www
-ls -al /app/static/www/css
+# python manage.py collectstatic --noinput
+# ls -al /app/static/
+# ls -al /app/static/www
+# ls -al /app/static/www/css
 python manage.py migrate
