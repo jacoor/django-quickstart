2172b9864fa5b619e2fa6fee9098510cac44445e
Jacek Ostański <jacek@ivolution.pl>
2021-08-08 18:08:44 +0200

    remove media

--- quickstart/settings.py
+++ quickstart/settings.py
@@ -160,8 +160,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
 django_heroku.settings(locals())
