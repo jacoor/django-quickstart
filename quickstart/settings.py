849fd5d66740dfd7a86e581c78bf56955f8ef012
Jacek Ostański <jacek@ivolution.pl>
2021-08-08 21:17:48 +0200

    fuck

--- quickstart/settings.py
+++ quickstart/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "/static/"
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
